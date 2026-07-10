97da473d47cbccd3f75a11b0bc3164deeb43aca9
Sujal Bhagat <sujalbhagatcodes@gmail.com>
2026-07-11 01:11:03 +0530

    string

--- basic code/hello.py
+++ basic code/hello.py
@@ -1,1 +1,6 @@
 print("Hello world")
+
+message="Hello world this is a varrable"
+print(message)
+message="Sujal"
+print(message)
---
+++ basic code/string.py
@@ -0,0 +1,4 @@
+name='sujal'
+print(name.title())
+print(name.upper())
+print(name.lower())
