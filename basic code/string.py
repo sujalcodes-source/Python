3f441155e6ec6d975af6c0d7535c23dabb1586fb
Sujal Bhagat <sujalbhagatcodes@gmail.com>
2026-07-13 14:34:46 +0530

    JULY 13 2026

--- basic code/string.py
+++ basic code/string.py
@@ -1,5 +1,9 @@
-name='sujal'
+name='sujal bhagat'
 print(name.title())
 print(name.upper())
 print(name.lower())
 print("sujal",6,7,"hello",sep="HTTP")
+print(name.startswith(name))
+print(name.split(name))
+print(name.find("sujal"))
+print(name.count(name))
