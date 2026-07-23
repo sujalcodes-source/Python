113551b09f4bbfe3aeebc2d7d16066883d545109
Sujal Bhagat <sujalbhagatcodes@gmail.com>
2026-07-23 22:49:00 +0530

    string method

---
+++ basic code/strip.py
@@ -0,0 +1,5 @@
+#this use different string method 
+name="                          SUJAL BHAGAT                                     "
+Name=name.strip()
+yname=input("Enter your name")
+print(f"This is my name {Name} What is your name {yname} ")
