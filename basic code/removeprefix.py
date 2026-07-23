cb0a505a5885f2e290a2a9a98e55718bbba10f8e
Sujal Bhagat <sujalbhagatcodes@gmail.com>
2026-07-23 23:14:14 +0530

    string method

---
+++ basic code/removeprefix.py
@@ -0,0 +1,5 @@
+# this is used to remove a prefix from the code also used most common to remove https
+website="https://www.google.com/"
+removeprefix=website.removeprefix("https://")
+
+print(removeprefix)
