f4af2fa6322f33a2340f5abaee01c4e296fb93a4
Sujal Bhagat <sujalbhagatcodes@gmail.com>
2026-07-30 06:11:39 +0530

    Create -ve.py

---
+++ practice problems/+ve/-ve.py
@@ -0,0 +1,7 @@
+num = int(input("Enter your number :"))
+if(num >0):
+    print(f"Number is positive , and this is your number  {num}")
+elif(num < 0):
+    print(f"Number is negative and this is your number {num}")
+else :
+    print(f"your number is zero , and this is your number {num}")
