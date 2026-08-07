cd90521910463148bea7cf54059b1aead1176745
Sujal Bhagat <sujalbhagatcodes@gmail.com>
2026-08-08 04:53:22 +0530

    Create password.py

---
+++ basic code/password.py
@@ -0,0 +1,7 @@
+password = input("Enter your password")
+yr_pass = "sujal"
+# use this == operation when we need to check a condition or a num . but mainly a condition 
+if (password == yr_pass):
+    print("your pass is correct deive opened")
+else:
+    print("wrong password")
