8dfb7b67fd61b83e1e9935428b1e792120c27ee5
Sujal Bhagat <sujalbhagatcodes@gmail.com>
2026-08-09 19:59:37 +0530

    Create detailprint.py

---
+++ basic code/detailprint.py
@@ -0,0 +1,13 @@
+name = input("ENTER YOUR NAME :")
+phone = int(input("ENTER YOUR PHONE NUMBER :"))
+age = int(input("ENTER YOUR AGE :"))
+
+def your_info ():
+    print(f"YOUR NAME IS {name}\n")
+    print(f"YOUR PHONE NUMBER IS {phone}\n")
+    print(f"YOUR AGE IS {age}\n")
+    print("THANK YOU")
+
+your_info()
+#this code takes name,phone,age as an input and and print the info using a  funcition the name of the function is your_info 
+#09 Aug 2026
