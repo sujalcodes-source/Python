36aeeeea4a85f6018a3c21911850b1a4e8c2fbad
Sujal Bhagat <sujalbhagatcodes@gmail.com>
2026-08-26 23:06:35 +0530

    Create sumof2num.py

---
+++ sumof2num.py
@@ -0,0 +1,10 @@
+# class Solution(object):
+#     def twoSum(self, nums, target):
+#         """
+#         :type nums: List[int]
+#         :type target: int
+#         :rtype: List[int]
+#         """
+num = [2,7,11,15] #target = 9 
+sum = num[0]+num[1]
+print(sum)
