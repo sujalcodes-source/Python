f5c23a99c62fa98ae15f1b0b5d92b8005bbdec72
Sujal Bhagat <sujalbhagatcodes@gmail.com>
2026-08-30 14:27:49 +0530

    Create p.py

---
+++ p.py
@@ -0,0 +1,8 @@
+def name():
+    fname = input("Enter your first name :")
+    lname = input("Enter your last name  :")
+    full_name = fname + lname
+    print(f"This your full name {full_name}")
+
+name()
+#calling the fucntion name() which will generate the name by taking first name and last name 
