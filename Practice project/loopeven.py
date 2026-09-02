3014446c42477496a671f9c6c882a6d804b3881a
Sujal Bhagat <sujalbhagatcodes@gmail.com>
2026-09-02 17:14:56 +0530

    Sep 02 2026

---
+++ Practice project/loopeven.py
@@ -0,0 +1,15 @@
+number = [1,2,3,4,5,6,7,8,9,10]
+for number in number:
+    if(number % 2 ==0):
+        print(f"The number is even {number}")
+
+# we can also improve the code by chaning the number list name as numbers which will 
+# dec the amount of confuse we get by in the above code 
+
+# BELOW IS THE MORE IMPROVED VERSION OF THE CODE
+
+# numbers = [1,2,3,4,5,6,7,8,9,10]
+
+# for number in numbers:
+#     if number % 2 == 0:
+#         print(f"The number is even {number}")
