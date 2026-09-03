b36c2791f292e5f90a47010c92414e70bbb83ebc
Sujal Bhagat <sujalbhagatcodes@gmail.com>
2026-09-03 20:01:34 +0530

    sep 03 2026

---
+++ Practice project/pn.py
@@ -0,0 +1,18 @@
+#this is code for finding +, - , zero 
+num = int(input("Enter your number :"))
+if (num>0):
+    print("THE NUMBER YOU ENTER IS POSITIVE")
+elif(num < 0):
+    print("THE NUMBER YOU ENTER IS NEGATIVE")
+else :
+    print("THE NUMBER YOU ENTER IS ZERO")
+
+
+# number = int(input("Enter a number: "))
+
+# if number > 0:
+#     print("The number is positive.")
+# elif number < 0:
+#     print("The number is negative.")
+# else:
+#     print("The number is zero.")
