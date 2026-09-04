0f75795d078775ee7a93262d4b3cf10d8b29ec02
Sujal Bhagat <sujalbhagatcodes@gmail.com>
2026-09-04 20:37:32 +0530

    Create findname.py

---
+++ practice problems/findname.py
@@ -0,0 +1,19 @@
+#this code will help you to find your name in a list 
+name = ["Sujal","Raju ","prajakta","ronaldo","rahully"]
+findname = input("Enter your name:")
+if(findname in  name):
+    print(f"Your name has been found {findname}")
+else:
+    print("Your name was not found")
+
+
+# name = ["Sujal", "Raju", "prajakta", "ronaldo", "rahully"]
+
+# findname = input("Enter your name: ")
+
+# if findname in name:
+#     print(f"Your name has been found: {findname}")
+# else:
+#     print("Your name was not found.")
+
+#correct answer above
