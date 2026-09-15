6a79c54408971a9685ff3dd0c1212e42351f6e2d
Sujal Bhagat <sujalbhagatcodes@gmail.com>
2026-09-15 20:28:33 +0530

    Create size.py

---
+++ basic code/size.py
@@ -0,0 +1,8 @@
+#float store the real number in double let see 
+import sys
+float = 3.14
+print(sys.getsizeof(float))
+# import sys
+
+# # Returns 24 bytes on 64-bit systems, 16 on 32-bit
+# print(sys.getsizeof(0.0))   
