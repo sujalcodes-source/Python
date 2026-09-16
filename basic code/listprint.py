c6c467ffb77d32ec05fb48703b4c22c56a535201
Sujal Bhagat <sujalbhagatcodes@gmail.com>
2026-09-16 19:14:13 +0530

    Create listprint.py

---
+++ basic code/listprint.py
@@ -0,0 +1,8 @@
+marks = int([10,20,30,40,50,60,70,80,90,100])
+for mark in marks:
+    if(marks > 28):
+        print("you have passed the exam")
+    elif(marks == 100 ):
+        print("You have top the exam")
+    else:
+        print("You got good marks ")
