d39ab32789696a9e1a3e3dd5383e0fcae13ee12c
Sujal Bhagat <sujalbhagatcodes@gmail.com>
2026-09-22 23:58:28 +0530

    Create largeof3.py

---
+++ largeof3.py
@@ -0,0 +1,15 @@
+# ### 🟡 Next Problem
+# Take **3 numbers** as input and print the **largest number**.
+# **Example:**
+# ```text
+# Enter numbers: 10 25 7
+# Largest: 25
+# ```
+# Use only `if`, `elif`, `else`.
+print("---PLEASE ENTER YOUR NUMBER---")
+no1 = int(input("Enter your 1 num"))
+no2 = int(input("Enter your 2 num"))
+no3 = int(input("Enter your 3 num"))
+
+if no1:
+
