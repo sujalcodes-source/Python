82e036b0111c06bc26dde5912cdbed267f16db96
Sujal Bhagat <sujalbhagatcodes@gmail.com>
2026-09-23 00:12:04 +0530

    SEP 23 2026

--- largeof3.py
+++ largeof3.py
@@ -6,10 +6,15 @@
 # Largest: 25
 # ```
 # Use only `if`, `elif`, `else`.
-print("---PLEASE ENTER YOUR NUMBER---")
-no1 = int(input("Enter your 1 num"))
-no2 = int(input("Enter your 2 num"))
-no3 = int(input("Enter your 3 num"))
+print("=== PLEASE ENTER YOUR NUMBER ===")
+no1 = int(input("Enter your 1 num :"))
+no2 = int(input("Enter your 2 num :"))
+no3 = int(input("Enter your 3 num :"))
 
-if no1:
-
+if ( no1 >no2 and no1 > no3):
+    print(no1)
+elif(no2 > no1 and no2 > no3 ):
+    print(no2)
+else:
+    print(no3)
+print("=== THANKS YOU :) ===")
